11aafa8b53056d751f6d93e667b796ca7943ec4d
nakyeong <yunna3425@naver.com>
2024-07-25 22:49:02 +0900

    feat: 사이드바

--- cheerstracker/src/index.js
+++ cheerstracker/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import SideBar from './components/SideBar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SideBar/>
   </React.StrictMode>
 );
