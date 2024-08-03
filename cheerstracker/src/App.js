c81566a3cd0360cc3233c973d1795b9ba50dea2f
Heeju1016 <huiju232@gmail.comcd >
2024-08-03 17:44:34 +0900

    app.js 기본구조 변경

--- cheerstracker/src/App.js
+++ cheerstracker/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Report from './pages/Report'
-import Calender from './pages/Calender'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/calender' element={<Calender />}/>
-            <Route path='/report' element={<Report />}/>
-        </Routes>
-    </BrowserRouter>
+    <div>
+      
+    </div>
   )
 }
 
